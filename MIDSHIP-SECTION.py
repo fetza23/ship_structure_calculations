02ec581679169f54cda9f745e41241ffaeb421d4
Fatih YegüL <fthygl.25323@gmail.com>
2023-11-13 03:16:05 +0300

    bracket floors drawn

--- MIDSHIP-SECTION.py
+++ MIDSHIP-SECTION.py
@@ -16,7 +16,7 @@ plt.plot(x,y ,color="b", label="inner bottom")
 
 y=np.linspace(1,ha/1000+1,1000)
 x=[0]*len(y)
-plt.plot(x,y,color="r",label="center girder")
+plt.plot(x,y,color="r",label="center girder",linewidth=tmcg/5)
 plt.legend()
 ################ SIDE GIRDER ###################
 if 4.5<=B/2:
@@ -29,6 +29,27 @@ if 4.5<=B/2:
             legend_added = True
         else:
             plt.plot(x, y, color="k", linewidth=tsg / 5)
+
+############### bracket floor ################
+x = [0,bBr/1000 , bBr/1000, 0]
+x1 =[0,-bBr/1000 , -bBr/1000, 0]
+y = [ 1,1, ha/1000+1, ha/1000+1]
+plt.plot(x1, y, label='bracket', color='y')
+plt.plot(x, y, color='y')
+
+x2=[B/2-bBr/1000 , B/2-bBr/1000, B/2]
+x3=[-B/2+bBr/1000 , -B/2+bBr/1000, -B/2]
+y = [1, ha/1000+1, ha/1000+1]
+plt.plot(x2, y, color='y')
+plt.plot(x3, y, color='y')
+
+bracket_hole1 = plt.Circle((bBr/2000,ha/2000+1), 0.15,fill=False, color='y')
+bracket_hole2 = plt.Circle((-bBr/2000,ha/2000+1), 0.15,fill=False, color='y')
+bracket_hole3 = plt.Circle((B/2-bBr/1750,ha/2000+1), 0.15,fill=False, color='y')
+bracket_hole4 = plt.Circle((-B/2+bBr/1750,ha/2000+1), 0.15,fill=False, color='y')
+bracket_hole=[bracket_hole3,bracket_hole2,bracket_hole4,bracket_hole1]
+for i in bracket_hole:
+    plt.gca().add_patch(i)
 
 ############ man hole #################
 if man_hole ==1:
