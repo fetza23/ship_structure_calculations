18dc6169bd65a517586b8069ca60132f65fc0edf
Fatih YegüL <fthygl.25323@gmail.com>
2023-11-26 00:10:11 +0300

    polyfit'i ayarı kaldı_ hidrostatic

--- hydrostatic_calculations.py
+++ hydrostatic_calculations.py
@@ -1,3 +1,5 @@
+
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -6,11 +8,11 @@ L = 74
 B = 10.89
 T=4.737
 D=7.1
-w=L*B*T*0.68*1.025
+CB=0.68
+
 
 
 df = pd.read_excel("yny.xlsx")
-print(df)
 
 if "WL0,3" in df.columns:
     WL05 = np.array([])
@@ -43,27 +45,15 @@ yeni_df = pd.concat(yeni_df_listesi, ignore_index=True)
 yeni_df = yeni_df.drop(columns=['postalar'])
 
 yeni_df = yeni_df.rename_axis("postalar")
-print(yeni_df)
-print(yeni_df.loc[70])
-
-print(yeni_df.columns[1:4])
-print(yeni_df.loc[0, "WL0"])
-print(yeni_df.loc[0, 'WL0,5'])
-print(yeni_df.loc[0,'WL1'])
-print(yeni_df.loc[74,'WL0'])
-print(yeni_df.loc[74,"WL0,5"])
-print(yeni_df.loc[74,'WL1'])
-print(yeni_df.loc[148, "WL0"])
-print(yeni_df.loc[148, 'WL0,5'])
-print(yeni_df.loc[148, 'WL1'])
-print("len yenidf",len(yeni_df))
-print("yeni_df.columns[1:4]",yeni_df.columns[1:4])
 
 
 
+#WL1 E KADAR OLAN ALAN VE MOMENTLER
 
 WL1Alan=[]
 SM=[0.5,2,0.5]
+WL1Moment=[]
+M_K=[0,0.5,1]
 for i in range(len(yeni_df)):  # print(f"{i}. postanın wl1 e kadar olan alan",a)
     liste=[]
     for col in yeni_df.columns[1:4]:
@@ -71,19 +61,22 @@ for i in range(len(yeni_df)):  # print(f"{i}. postanın wl1 e kadar olan alan",a
     liste_ps=pd.Series(liste)
     carpim=SM*liste_ps
     a=sum(carpim)
-    b=2/3*T/4*a
+    b=2/3*T/8*a
+    Mcarpim = M_K * carpim
+    c=sum(Mcarpim)
+    d=2/3*(T/8)**2*c
+    WL1Moment.append(d)
     WL1Alan.append(b)
+WL1Moment_PDS=pd.Series(WL1Moment)
 WL1Alan_PDS=pd.Series(WL1Alan)
-print("WL1 0. POSTA ",WL1Alan_PDS.loc[0])
-print("WL1 74. POSTA ",WL1Alan_PDS.loc[74])
-print("WL1 148. POSTA ",WL1Alan_PDS.loc[148])
 
-
+#WL2 E KADAR OLAN ALAN VE MOMENTLER
 
 
 SM=[1,4,1]
-
 WL2Alan=[]
+WL2Moment=[]
+M_K=[0,1,2]
 for i in range(len(yeni_df)):  # print(f"{i}. postanın wl2 e kadar olan alan",a)
     liste=[]
     for col in yeni_df[["WL0","WL1","WL2"]]:
@@ -92,15 +85,20 @@ for i in range(len(yeni_df)):  # print(f"{i}. postanın wl2 e kadar olan alan",a
     carpim=SM*liste_ps
     a=sum(carpim)
     b=2/3*T/4*a
+    Mcarpim = M_K * carpim
+    c = sum(Mcarpim)
+    d = 2 / 3 * (T / 4) ** 2 * c
+    WL2Moment.append(d)
     WL2Alan.append(b)
+WL2Moment_PDS=pd.Series(WL2Moment)
 WL2Alan_PDS=pd.Series(WL2Alan)
-print("WL2 0. POSTA ",WL2Alan_PDS.loc[0])
-print("WL2 74. POSTA ",WL2Alan_PDS.loc[74])
-print("WL2 148. POSTA ",WL2Alan_PDS.loc[148])
 
+#WL3 E KADAR OLAN ALAN VE MOMENTLER
 
 WL3Alan=[]
 SM=[0.5,2,1.5,4,1]
+WL3Moment=[]
+M_K=[0,0.5,1,2,3]
 for i in range(len(yeni_df)):  # print(f"{i}. postanın wl3 e kadar olan alan",a)
     liste=[]
     for col in yeni_df[["WL0","WL0,5","WL1","WL2","WL3"]]:
@@ -109,15 +107,20 @@ for i in range(len(yeni_df)):  # print(f"{i}. postanın wl3 e kadar olan alan",a
     carpim=SM*liste_ps
     a=sum(carpim)
     b=2/3*T/4*a
+    Mcarpim = M_K * carpim
+    c = sum(Mcarpim)
+    d = 2 / 3 * (T / 4) ** 2 * c
+    WL3Moment.append(d)
     WL3Alan.append(b)
 WL3Alan_PDS=pd.Series(WL3Alan)
-print("WL3 0. POSTA ",WL3Alan_PDS.loc[0])
-print("WL3 74. POSTA ",WL3Alan_PDS.loc[74])
-print("WL3 148. POSTA ",WL3Alan_PDS.loc[148])
+WL3Moment_PDS=pd.Series(WL3Moment)
 
+#WL4 E KADAR OLAN ALAN VE MOMENTLER
 
 WL4Alan=[]
 SM=[1,4,2,4,1]
+WL4Moment=[]
+M_K=[0,1,2,3,4]
 for i in range(len(yeni_df)):  #rint(f"{i}. postanın wl4 e kadar olan alan",a)
     liste=[]
     for col in yeni_df[["WL0","WL1","WL2","WL3","WL4"]]:
@@ -126,13 +129,20 @@ for i in range(len(yeni_df)):  #rint(f"{i}. postanın wl4 e kadar olan alan",a)
     carpim=SM*liste_ps
     a=sum(carpim)
     b=2/3*T/4*a
+    Mcarpim = M_K * carpim
+    c = sum(Mcarpim)
+    d = 2 / 3 * (T / 4) ** 2 * c
+    WL4Moment.append(d)
     WL4Alan.append(b)
 WL4Alan_PDS=pd.Series(WL4Alan)
-print("WL4 0. POSTA ",WL4Alan_PDS.loc[0])
-print("WL4 74. POSTA ",WL4Alan_PDS.loc[74])
-print("WL4 148. POSTA ",WL4Alan_PDS.loc[148])
+WL4Moment_PDS=pd.Series(WL4Moment)
+
+#WL5 E KADAR OLAN ALAN VE MOMENTLER
+
 WL5Alan=[]
 SM=[0.5,2,1.5,4,2,4,1]
+WL5Moment=[]
+M_K=[0,0.5,1,2,3,4,5]
 for i in range(len(yeni_df)):  # print(f"{i}. postanın wl5 e kadar olan alan",a)
     liste=[]
     for col in yeni_df[["WL0","WL0,5","WL1","WL2","WL3","WL4","WL5"]]:
@@ -141,14 +151,21 @@ for i in range(len(yeni_df)):  # print(f"{i}. postanın wl5 e kadar olan alan",a
     carpim=SM*liste_ps
     a=sum(carpim)
     b=2/3*T/4*a
+    Mcarpim = M_K * carpim
+    c = sum(Mcarpim)
+    d = 2 / 3 * (T / 4) ** 2 * c
+    WL5Moment.append(d)
     WL5Alan.append(b)
 WL5Alan_PDS=pd.Series(WL5Alan)
-print("WL5 0. POSTA ",WL5Alan_PDS.loc[0])
-print("WL5 74. POSTA ",WL5Alan_PDS.loc[74])
-print("WL5 148. POSTA ",WL5Alan_PDS.loc[148])
+WL5Moment_PDS=pd.Series(WL5Moment)
+
+#WL6 E KADAR OLAN ALAN VE MOMENTLER
+
 
 WL6Alan=[]
 SM=[1,4,2,4,2,4,1]
+WL6Moment=[]
+M_K=[0,1,2,3,4,5,6]
 for i in range(len(yeni_df)):  # print(f"{i}. postanın wl6 e kadar olan alan",a)
     liste = []
     for col in yeni_df[["WL0","WL1","WL2","WL3","WL4","WL5","WL6"]]:
@@ -157,35 +174,27 @@ for i in range(len(yeni_df)):  # print(f"{i}. postanın wl6 e kadar olan alan",a
     carpim=SM*liste_ps
     a=sum(carpim)
     b=2/3*T/4*a
-
+    Mcarpim = M_K * carpim
+    c = sum(Mcarpim)
+    d = 2 / 3 * (T / 4) ** 2 * c
+    WL6Moment.append(d)
     WL6Alan.append(b)
 WL6Alan_PDS=pd.Series(WL6Alan)
-print("WL6 0. POSTA ",WL6Alan_PDS.loc[0])
-print("WL6 74. POSTA ",WL6Alan_PDS.loc[74])
-print("WL6 148. POSTA ",WL6Alan_PDS.loc[148])
-
+WL6Moment_PDS=pd.Series(WL6Moment)
 
 
 
 alanlar = pd.concat([WL1Alan_PDS, WL2Alan_PDS, WL3Alan_PDS, WL4Alan_PDS, WL5Alan_PDS, WL6Alan_PDS], axis=1)
 alanlar.columns = ['WL1Alan_PDS', 'WL2Alan_PDS', 'WL3Alan_PDS', 'WL4Alan_PDS', 'WL5Alan_PDS', 'WL6Alan_PDS']
-#print(alanlar.loc[76])
+momentler=pd.concat([WL1Moment_PDS, WL2Moment_PDS, WL3Moment_PDS, WL4Moment_PDS, WL5Moment_PDS, WL6Moment_PDS], axis=1)
+momentler.columns = ['WL1_Moment', 'WL2_Moment', 'WL3_Moment', 'WL4_Moment', 'WL5_Moment', 'WL6_Moment']
+
 
 
 def moment_kolu():
     MK = list(range(-(len(yeni_df)-1)// 2, 0))  + list(range((len(yeni_df)+1)//2))
     MK=pd.Series(MK)
     return MK
-
-WL6_Moment=WL6Alan_PDS*moment_kolu()
-WL5_Moment=WL5Alan_PDS*moment_kolu()
-WL4_Moment=WL4Alan_PDS*moment_kolu()
-WL3_Moment=WL3Alan_PDS*moment_kolu()
-WL2_Moment=WL2Alan_PDS*moment_kolu()
-WL1_Moment=WL1Alan_PDS*moment_kolu()
-momentler = pd.concat([WL1_Moment, WL2_Moment, WL3_Moment, WL4_Moment, WL5_Moment, WL6_Moment], axis=1)
-momentler.columns = ['WL1_Moment', 'WL2_Moment', 'WL3_Moment', 'WL4_Moment', 'WL5_Moment', 'WL6_Moment']
-
 
 
 def YG(T=2*D/3):
@@ -200,9 +209,7 @@ def YG(T=2*D/3):
     yari_genislik=pd.Series(yari_genislik)
     return yari_genislik
 
-
-
-def Alan(T=2*D/3):
+def Alan(T):
     T_A_list = []
     for i in range(len(alanlar)):
         ypd = pd.DataFrame(alanlar.loc[i])
@@ -214,7 +221,7 @@ def Alan(T=2*D/3):
     T_A_list=pd.Series(T_A_list)
     return T_A_list
 
-def moment(T=2*D/6):
+def moment(T):
     T_M_list = []
     for i in range(len(momentler)):
         ypd = pd.DataFrame(momentler.loc[i])
@@ -238,122 +245,114 @@ def SM_creator(nmr):
     return SM
 
 
-def Bwl():
-    Bwl=max(YG()*2)
+def Bwl(T):
+    Bwl=max(YG(T)*2)
     return Bwl
 
-def carpim1():
-    carpim1=Alan()*SM_creator(Alan())
+def carpim1(T):
+    carpim1=Alan(T)*SM_creator(Alan(T))
     return carpim1
-def carpim2():
-    carpim2=moment_kolu()*carpim1()
+def carpim2(T):
+    carpim2=moment_kolu()*carpim1(T)
     return carpim2
-def carpim3():
-    carpim3=moment()*SM_creator(moment())
+def carpim3(T):
+    carpim3=moment(T)*SM_creator(moment(T))
     return carpim3
-def carpim4():
+def carpim4(T):
     carpim4=SM_creator(yeni_df)*YG(T)
     carpim4=pd.Series(carpim4)
     return carpim4
-def carpim5():
-    carpim5=moment_kolu()*carpim4()
+def carpim5(T):
+    carpim5=moment_kolu()*carpim4(T)
     carpim5=pd.Series(carpim5)
     return carpim5
-def carpim6():
-    carpim6=moment_kolu()*carpim5()
+def carpim6(T):
+    carpim6=moment_kolu()*carpim5(T)
     return carpim6
-def carpim7():
-    carpim7=YG()**3*SM_creator(yeni_df)
+def carpim7(T):
+    carpim7=YG(T)**3*SM_creator(yeni_df)
     return carpim7
 
 
 
-
-def DV(s=0.5):
-    DV=1/3*s*sum(carpim1())
+def DV(T,s=0.5):
+    DV=1/3*s*sum(carpim1(T))
     return DV
 
-def DW(s=0.5):
-    DW=DV()*1.025
+def DW(T):
+    DW=DV(T)*1.025
     return DW
-def LCB(s=0.5):
-    LCB=s*sum(carpim2())/sum(carpim1())
+def LCB(T,s=0.5):
+    LCB=s*sum(carpim2(T))/sum(carpim1(T))
     return LCB
-def KB():
-    KB=sum(carpim3())/sum(carpim1())
+def KB(T):
+    KB=sum(carpim3(T))/sum(carpim1(T))
     return KB
 def CB(T=2*D/3):
-    CB=DV()/(L*Bwl()*T)
+    CB=DV(T)/(L*Bwl(T)*T)
     return CB
 def CM(T=2*D/3):
-    CM=Alan().max()/(Bwl()*T)
+    CM=Alan(T).max()/(Bwl(T)*T)
     return CM
-def CP():
-    CP=CB()/CM()
+def CP(T):
+    CP=CB(T)/CM(T)
     return CP
 
-
-
-
-
-
-
-def LCF(s=0.5):
-    LCF=s*sum(carpim5())/sum(carpim4())
+def LCF(T,s=0.5):
+    LCF=s*sum(carpim5(T))/sum(carpim4(T))
     return LCF
 
-def CWP():
-    CWP=AWP()/(Bwl()*L)
+def CWP(T):
+    CWP=AWP(T)/(Bwl(T)*L)
     return CWP
-def AWP(s=0.5):
-    AWP=2/3*s*sum(carpim4())
+def AWP(T,s=0.5):
+    AWP=2/3*s*sum(carpim4(T))
     return AWP
-def TPC(ro=1.025):
-    TPC=AWP()/100*ro
+def TPC(T):
+    TPC=AWP(T)/100*1.025
     return TPC
-def ix(s=0.5):
-    ix=2/9*s*sum(carpim7())
+def ix(T,s=0.5):
+    ix=2/9*s*sum(carpim7(T))
     return ix
 
-def BM():
-    BM=ix()/DV()
+def BM(T):
+    BM=ix(T)/DV(T)
     return BM
 
-def i0(s=0.5):
-    i0=2/3*s**3*sum(carpim6())
+def i0(T,s=0.5):
+    i0=2/3*s**3*sum(carpim6(T))
     return i0
 
-def i_f(s=0.5):
-    i_f=i0()-AWP()*LCF()**2
+def i_f(T):
+    i_f=i0(T)-AWP(T)*LCF(T)**2
     return i_f
 
-def BML(s=0.5):
-    BML=i_f()/DV()
+def BML(T):
+    BML=i_f(T)/DV(T)
     return BML
 
-def MT1():
-    MT1=DW()*BML()/L
+def MT1(T):
+    MT1=DW(T)*BML(T)/L
     return MT1
 
 
-print("dv",DV())
-print("dw",DW())
-print("lcb",LCB())
-print("kb",KB())
-print("cb",CB())
-print("CM",CM())
-print("CP",CP())
-print("i_f",i_f())
-print("i0",i0())
-print("ix",ix())
-print("BM",BM())
-print("LCF",LCF())
-print("BML",BML())
-print("MT1",MT1())
-print("TPC",TPC())
-print("AWP",AWP())
-print("CWP",CWP())
-
+print("dv",DV(T))
+print("dw",DW(T))
+print("lcb",LCB(T))
+print("kb",KB(T))
+print("cb",CB(T))
+print("CM",CM(T))
+print("CP",CP(T))
+print("AWP",AWP(T))
+print("LCF",LCF(T))
+print("TPC",TPC(T))
+print("CWP",CWP(T))
+print("ix",ix(T))
+print("BM",BM(T))
+print("i0",i0(T))
+print("i_f",i_f(T))
+print("BML",BML(T))
+print("MT1",MT1(T))
 
 
 
